c9eb2ecc4af60b7dd21b52cec5e93777b9fb4281
Patrick Poitras <42386710+Patrick-Poitras@users.noreply.github.com>
2022-08-20 19:31:36 -0700

    Revert to contrast scheme. More chill in a side-by-side. Also some more color selections.

--- bright-tungsten-theme.el
+++ bright-tungsten-theme.el
@@ -2,19 +2,20 @@
 
 (autothemer-deftheme
  bright-tungsten
- "A theme mostly inspired by the VSCode theme 'Community Material Themes -- Ocean High Contrast'"
+ "A theme mostly inspired by the VSCode theme 'Community Material Themes - Ocean High Contrast'"
 
  ((((class color) (min-colors #xffffff)))
 
   ;; Color palette definition.
 
   ;; Element colors.
-  (EditorBackground "#0F111A")
-  ;;(EditorForeground "#8F93A2")
-  (EditorForeground "gray80")
-  (SelectionBackground "#80CBC4")
-  (TabBarBackground    "#090B10")
-  (TabBarForeground    "#4B526D")
+  (EditorBackground     "#0F111A")
+  (EditorForeground "#8F93A2")
+  ;;(EditorForeground     "gray80")
+  (CommentsForeground "gray35")
+  (SelectionBackground  "#80CBC4")
+  (ModeBarBackground    "#090B10")
+  (ModeBarForeground    "#4B526D")
 
   (Highlight-Orange "DarkOrange2")
   (Highlight-Gray   "LightSteelBlue")
@@ -29,39 +30,37 @@
   (Yellow        "#FFCB6B")
   (BrightBlack   "#464B5D")
 
-  (DeepYellow "#FAA452")
-  (PaleRed "#FF7777")
-  (PaleOrange "#F78C6C")
+  (DeepYellow    "#FAA452")
+  (PaleRed       "#FF7777")
+  (PaleOrange    "#F78C6C")
 
-  ;;(magenta "#FF00FF")
+  ;;(magenta "#FF00FF") ;; ugly color for debugging.
   )
 
  ;; Assign colors to faces.
- ((default (:foreground EditorForeground :background EditorBackground)) ;; Make fg brighter?
+ ((default (:foreground EditorForeground :background EditorBackground))
   (cursor  (:background Yellow))
-  (mode-line (:foreground Highlight-Gray :background TabBarBackground
-			  ;;:box Highlight-Orange
-			  ))
-  (mode-line-inactive (:foreground TabBarForeground :background TabBarBackground))
+  (region  (:background SelectionBackground))
+  
+  (mode-line (:foreground Highlight-Gray :background ModeBarBackground))
+  (mode-line-inactive (:foreground ModeBarForeground :background ModeBarBackground))
   (font-lock-warning-face (:foreground Red :weight 'bold))
   (font-lock-keyword-face (:foreground Cyan :slant 'italic :weight 'bold))
   (font-lock-function-name-face (:foreground Yellow))
   (font-lock-type-face (:foreground Green))
   (font-lock-builtin-face (:foreground Blue))
-  (font-lock-comment-face (:foreground "gray55"))
+  (font-lock-comment-face (:foreground CommentsForeground))
   (font-lock-string-face (:foreground Green))
   (font-lock-constant-face (:foreground PaleRed))
   (font-lock-variable-name-face (:foreground DeepYellow))
+
+  (hl-line (:background ModeBarBackground))
+
+  ;; Eshell definitions
+  (eshell-prompt (:foreground Blue))
+  (eshell-ls-backup (:foreground "gray50"))
+  (eshell-ls-directory (:foreground "gold"))
   )
  )
- 
-(provide-theme 'bright-tungsten)
 
-;font-lock-builtin-face
-;font-lock-comment-delimiter-face 	font-lock-comment-face
-;font-lock-constant-face 	font-lock-doc-face
-;font-lock-doc-markup-face 	font-lock-function-name-face
-;font-lock-keyword-face 	font-lock-negation-char-face
-;font-lock-preprocessor-face 	font-lock-regexp-grouping-backslash
-;font-lock-regexp-grouping-construct 	font-lock-string-face
-;font-lock-type-face 	font-lock-variable-name-face
+(provide-theme 'bright-tungsten)
